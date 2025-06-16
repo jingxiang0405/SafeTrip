e8b9c0784f0a47823b209a63dbc02f68d889f5a0
jingxiang0405 <74401869+jingxiang0405@users.noreply.github.com>
2025-06-16 18:17:33 +0800

    [refactor] all stop return type

--- backend/src/controllers/bus_controller.js
+++ backend/src/controllers/bus_controller.js
@@ -39,7 +39,7 @@ async function GetAllStops(req, res) {
         data.forEach((dirData) => {
             const stops = [];
             dirData.Stops.forEach((stop) => {
-                stops.push(stop.StopName.Zh_tw);
+                stops.push({ name: stop.StopName.Zh_tw, location: { lon: stop.StopPosition.PositionLon, lat: stop.StopPosition.PositionLat } });
             })
 
             result[dirData.Direction] = stops;
